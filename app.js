cecf59b50068da32c04b65e2bb3a2d9967858740
Thomas <thomas.boudru@hotmail.com>
2024-04-13 16:11:07 +0200

    version

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ require('./models/connection');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var emailsRouter = require('./routes/emails');
+var termsRouter = require('./routes/terms');
 
 var app = express();
 
@@ -25,5 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/emails', emailsRouter);
+app.use('/terms', termsRouter);
 
 module.exports = app;
