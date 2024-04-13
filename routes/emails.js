3c2b51fd48232aa13ce400dc76c2dad7da09cfc1
Thomas <thomas.boudru@hotmail.com>
2024-04-13 14:10:41 +0200

    version

--- routes/emails.js
+++ routes/emails.js
@@ -5,18 +5,18 @@ const Email = require('../models/emails')
 
 // Route POST add an email to list
 
-router.post('/listEmail', function(req, res) {
+router.post('/listEmail', async function(req, res) {
     const { email } = req.body;
+  
+    try {
+      const newEmail = new Email({ email : email });
+  
+      await newEmail.save();
 
-    const newEmail = new Email({ email });
-
-    newEmail.save(function(err) {
-      if (err) {
-        return res.status(500).json({ result: false, message: "Email error" });
-      }
-      // Répondre avec un message de succès
-      res.status(201).json({ result: true, message: "Email success" });
-    });
+      res.status(201).json({ result : true, message: "Email success" });
+    } catch (err) {
+      res.status(500).json({ result : false,  message: "Email error" });
+    }
   });
 
 
