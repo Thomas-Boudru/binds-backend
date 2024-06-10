4d85c2a65dce24157d406213efe1f11178f693f1
Thomas <thomas.boudru@hotmail.com>
2024-06-10 09:34:25 +0200

    version

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var emailsRouter = require('./routes/emails');
 var termsRouter = require('./routes/terms');
-var puppeteerRouter = require('./routes/puppeteer');
+var activitiesRouter = require('./routes/activities');
 
 var app = express();
 
@@ -28,6 +28,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/emails', emailsRouter);
 app.use('/terms', termsRouter);
-app.use('/puppeteer', puppeteerRouter);
+app.use('/activities', activitiesRouter);
 
 module.exports = app;
