72c2bf325c1b48db108c78d5370b4dfe16ca4656
Thomas <thomas.boudru@hotmail.com>
2024-06-10 10:15:46 +0200

    version

--- routes/activities.js
+++ routes/activities.js
@@ -1,20 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch');
-const puppeteer = require('puppeteer');
+const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
 
 // Fonction pour extraire le texte d'un site web
 async function extractTextFromPage(url) {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+    });
+    
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: 'domcontentloaded' });
     
+    // Extraire le texte du body de la page
     const pageText = await page.evaluate(() => {
         return document.body.innerText;
     });
 
     await browser.close();
 
+    // Trim et limiter à 1000 caractères
     const trimmedText = pageText.replace(/\s+/g, ' ').trim();
     const limitedText = trimmedText.substring(0, 1000);
 
@@ -33,8 +43,6 @@ router.post('/summarize', async (req, res) => {
         // Extraire le texte de la page
         const extractedText = await extractTextFromPage(pageUrl);
 
-        console.log("extractedText",extractedText)
-
         // Faire une requête à l'API de ChatGPT
         const response = await fetch('https://api.openai.com/v1/chat/completions', {
             method: 'POST',
@@ -46,14 +54,14 @@ router.post('/summarize', async (req, res) => {
                 model: "gpt-3.5-turbo",
                 messages: [
                     { role: "system", content: "You are a specialist in writing company descriptions." },
-                    { role: "user", content: `You have to accurately present the company in maximum 5 sentences in ${language} following those informations : ${extractedText}. Please use third-person perspective and provide an external view.` }
-                ],
+                    { role: "user", content: `You have to accurately present the company in maximum 5 sentences in ${language} following those informations: ${extractedText}. Please use third-person perspective and provide an external view.` }
+                ]
             })
         });
 
         const responseData = await response.json();
 
-        res.json({result: true, data : responseData.choices[0].message.content});
+        res.json(responseData);
     } catch (error) {
         console.error("Erreur lors de la demande à l'API de ChatGPT :", error);
         res.status(500).json({ error: "Une erreur s'est produite lors de la demande à l'API de ChatGPT." });
